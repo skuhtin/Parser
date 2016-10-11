7c41003d3a942d8c8bdafd64e43b746300721fd9
Kuhtin Sergey <kuhtin.sv@gmail.com>
2016-10-11 14:03:15 +0300

    commit with vetka1

--- src/main/java/ua/codegym/impl/XmlParser.java
+++ src/main/java/ua/codegym/impl/XmlParser.java
@@ -8,28 +8,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-public class XmlParser implements Parser{
+//azzazza
 
+public class XmlParser implements Parser {
 
-  private final Map<Event, EventHandler> handlers;
+    private final Map<Event, EventHandler> handlers;
 
-  public XmlParser(Map<Event, EventHandler> handlers) {
-    this.handlers = handlers;
-  }
-
-  @Override
-  public void parse(InputStream in) throws IOException{
-    Event event = Event.START_ELEMENT;
-    int countLineSymb = 0;
-    int countLines = 1;
-    while (in.available() > 0) {
-      int tmp = in.read();
-      event = event.next(handlers,tmp,countLineSymb,countLines);
-      countLineSymb++;
-      if (tmp == 10) {
-        countLines++;
-      }
-
+    public XmlParser(Map<Event, EventHandler> handlers) {
+        this.handlers = handlers;
     }
-  }
+
+    @Override
+    public void parse(InputStream in) throws IOException {
+        Event event = Event.START_ELEMENT;
+        int countLineSymb = 0;
+        int countLines = 1;
+        while (in.available() > 0) {
+            int tmp = in.read();
+            event = event.next(handlers, tmp, countLineSymb, countLines);
+            countLineSymb++;
+            if (tmp == 10) {
+                countLines++;
+            }
+
+        }
+    }
 }
